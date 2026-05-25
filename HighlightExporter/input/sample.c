6bb1496248d65327071496a907256e3a2ae61d0b
羽翩跹 <yupianxian@outlook.com>
2026-05-25 18:54:45 +0800

    Add HighlightExporter/input/sample.c [batch-upload]

---
+++ HighlightExporter/input/sample.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* create_node(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void print_list(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    Node* head = create_node(1);
+    head->next = create_node(2);
+    head->next->next = create_node(3);
+
+    print_list(head);
+    return 0;
+}
